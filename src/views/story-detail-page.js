4c5f8f4fc8aa1c661220485ab07137b2d903ba93
ammar arief <ammararief321@gmail.com>
2025-05-16 13:29:04 +0700

    fix mark

--- src/views/story-detail-page.js
+++ src/views/story-detail-page.js
@@ -77,7 +77,19 @@ export function renderStoryDetailPage(story) {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: '© OpenStreetMap contributors'
         }).addTo(map);
-        L.marker([story.lat, story.lon])
+        // Custom SVG icon for marker
+        const customIcon = L.divIcon({
+          html: `
+            <svg width="36" height="48" viewBox="0 0 36 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M18 0C8.064 0 0 8.064 0 18C0 31.5 18 48 18 48C18 48 36 31.5 36 18C36 8.064 27.936 0 18 0ZM18 24.3C14.508 24.3 11.7 21.492 11.7 18C11.7 14.508 14.508 11.7 18 11.7C21.492 11.7 24.3 14.508 24.3 18C24.3 21.492 21.492 24.3 18 24.3Z" fill="#B03052"></path>
+            </svg>
+          `,
+          className: '',
+          iconSize: [36, 48],
+          iconAnchor: [18, 48],
+          popupAnchor: [0, -48]
+        });
+        L.marker([story.lat, story.lon], { icon: customIcon })
           .addTo(map)
           .bindPopup(story.name)
           .openPopup();
